789c6ff6a0ee7612239689fd2e5c92f0aeece233
Nashipai <engineerbetsy96@gmail.com>
2026-07-23 21:09:12 +0300

    initial commit

---
+++ day04_assignment.py
@@ -0,0 +1,41 @@
+first_name = "John"
+last_name = "Doe"
+age = 20
+height = "1.75"
+course = "Software engineering"
+university = "Zetech University"
+year_of_study = 2026
+favourite_programming_language = "python"
+dream_company = "Software company"
+dream_job = "softawere engineer"
+owns_a_laptop = True
+has_programming_experience = True
+
+#Display Your Profile
+print("======STUDENT PROFILE======")
+print(first_name)
+print(last_name)
+print(age)
+print(height)
+print(course)
+print(university)
+print(year_of_study)
+print(favourite_programming_language)
+print(dream_job)
+print(dream_company)
+print(owns_a_laptop)
+print(has_programming_experience)
+
+#inspect data types
+print(type(first_name))
+print(type(last_name))
+print(type(age))
+print(type(height))
+print(type(course))
+print(type(university))
+print(type(year_of_study))
+print(type(favourite_programming_language))
+print(type(dream_job))
+print(type(dream_company))
+print(type(owns_a_laptop))
+print(type(has_programming_experience))
