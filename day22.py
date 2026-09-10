2bbdd69a99f418401d444a6e409596adf9b17cb9
Nashipai <engineerbetsy96@gmail.com>
2026-09-10 15:58:55 +0300

    Day22 lesson

---
+++ day22.py
@@ -0,0 +1,26 @@
+#Algorithmic thinking
+#Data structures and algorithms
+#Primitive data structures
+#problem and a solution process
+
+# BMI calculator
+#input
+height = 5.5
+weight = 50
+
+#process
+bmi = weight / (height)
+
+#output
+print("Your BMI is:", bmi)
+
+#pseudocode -- eplaining the steps of the BMI calculator
+#START
+ #Input height and weight
+ #Calculate BMI
+ #Output BMI
+#END
+
+
+
+
